a517be9ff60dea767915cd6cafd53fedbab7a57a
ShobhitSingh11 <139750384+ShobhitSingh11@users.noreply.github.com>
2025-02-27 11:25:11 +0530

    Update CoverageCheckerTest.groovy

--- src/test/groovy/org/scoverage/CoverageCheckerTest.groovy
+++ src/test/groovy/org/scoverage/CoverageCheckerTest.groovy
@@ -24,7 +24,7 @@ class CoverageCheckerTest {
     @Rule
     public TemporaryFolder tempDir = new TemporaryFolder()
 
-    // error when report file is not there
+    // error when report file is not there ok
 
     @Test
     void failsWhenReportFileIsNotFound() {
